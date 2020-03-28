b8ba6c4105b9b7416d409e633536e5677e63a26b
asia-tango <kaplunoleksandra@gmail.com>
2020-03-28 22:11:22 +0200

    Directive *ngFor

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class CarsComponent {
   carName = '';
   addCarStatus = false;
+  cars: string[] = ['Ford', 'Mazda', 'Audi'];
+  items = [
+    {id: 3, name: 'item 1'},
+    {id: 7, name: 'item 2'},
+    {id: 5, name: 'item 3'}
+  ]
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   }
 
 }
