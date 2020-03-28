abe3f7f4b85f0e1589a49fad0b55c7a680a39aa5
asia-tango <kaplunoleksandra@gmail.com>
2020-03-28 20:54:08 +0200

    Work with input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Theory';
+  inputText = '';
+
+  onKeyUp(event) {
+    this.inputText = event.target.value;
+    event.target.value = '';
+  }
 }
